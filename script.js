86303987cece741ab9c6f12cb8c63234695259a1
TDL2627 <the.dark.lord.2627@gmail.com>
2022-01-11 13:09:02 +0200

    addded modal

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ console.log(canibus)
 countCanibus = data => {
   var count = document.getElementById('counter');
   if (data) {
-    count.innerHTML = 'There are a total of ' + data + ' items';
+    count.innerHTML = 'There is a total of ' + data + ' different dagga.';
     // Show the heading text for the table
     document.getElementById('name').style.display = 'block';
   } else {
@@ -193,4 +193,32 @@ getCanibus();
 // Close Edit form
 closeInput = () => {
   document.getElementById('editForm').style.display = 'none';
+}
+
+// modal1
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
